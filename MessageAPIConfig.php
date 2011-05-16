ce990fca471c8cba064fe124ed7c40f4c7497f62
nickwilder <nicholaswilder@yahoo.com>
2011-05-16 14:52:04 -0700

    Account Id is no longer used.

--- MessageAPIConfig.php
+++ MessageAPIConfig.php
@@ -3,7 +3,6 @@
 /// Message API Configuration File
 
 define('MESSAGE_API_ACCOUNTNAME', '%ACCOUNTNAME%');
-define('MESSAGE_API_ACCOUNTID', '%ACCOUNTID%');
 define('MESSAGE_API_KEY', '%APIKEY%'); // this is secret!  DO NOT SHARE/PUBLISH!
 
 ?>
